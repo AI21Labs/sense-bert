7b4574b82a5599b77fdac3d7ca6ff1b232b1591c
Yoel Zeldes <yoelz@ai21.com>
2020-07-09 17:53:27 +0300

    clean code

--- README.md
+++ README.md
@@ -2,16 +2,16 @@
 
 This is the code for loading the SenseBERT model, described in [our paper](https://www.aclweb.org/anthology/2020.acl-main.423.pdf) from ACL 2020.
 
-## Available Models
+## Available models
 
 We made two SenseBERT models public:
 
 * sensebert-base-uncased
 * sensebert-large-uncased
 
-These models share the same number of parameters as Google's BERT models, except for the following (both changes are described in our paper thoroughly):
-1. We use a larger vocabulary
-2. We add a supersense prediction head at pre-training. The sense embeddings are also used as inputs to the model. 
+These models have the same number of parameters as Google's BERT models, except for the following (both changes are described in our paper thoroughly):
+1. We use a larger vocabulary.
+2. We add a supersense prediction head. The sense embeddings are also used as inputs to the model. 
 
 ## Requirements
 
@@ -19,29 +19,33 @@ These models share the same number of parameters as Google's BERT models, except
 * TensorFlow 1.15
 * NLTK
 
+You can install these using:
+```bash
+pip install -r requirements.txt
+```
+
 ## Usage
 
 
-### Supersense and MLM Predictions
+### Supersense and MLM predictions
 
-This is an example for making Masked Language Modeling (MLM) and Supersense predictions based on SenseBERT:
+This is an example for making Masked Language Modeling (MLM) and supersense predictions based on SenseBERT:
 ```python
 import tensorflow as tf
 from sensebert import SenseBert
 
 with tf.Session() as session:
-    sensebert_model = SenseBERT("sensebert-base-uncased", session=session)  # or sensebert-large-uncased
+    sensebert_model = SenseBert("sensebert-base-uncased", session=session)  # or sensebert-large-uncased
     input_ids, input_mask = sensebert_model.tokenize(["I went to the store to buy some groceries.", "The store was closed."])
     model_outputs = sensebert_model.run(input_ids, input_mask)
 
-contextualized_embeddings, mlm_logits, supersense_logits = model_outputs  # These are NumPy arrays
+contextualized_embeddings, mlm_logits, supersense_logits = model_outputs  # these are NumPy arrays
 ```
-Note that both vocabularies (words and supersenses) are available for you via ```sensebert_model.tokenizer```. For example, in order to predict the supersense of the word 'groceries' in the above example, you may run
+Note that both vocabularies (tokens and supersenses) are available for you via ```sensebert_model.tokenizer```. For example, in order to predict the supersense of the word 'groceries' in the above example, you may run
 ```python
 import numpy as np
 
-tokenizer = sensebert_model.tokenizer
-print(tokenizer.convert_ids_to_senses([np.argmax(supersense_logits[0][9])]))
+print(sensebert_model.tokenizer.convert_ids_to_senses([np.argmax(supersense_logits[0][9])]))
 ```
 This will output:
 ```
@@ -49,12 +53,12 @@ This will output:
 ```
 ### Fine-tuning
 
-If you want to fine-tune SenseBERT, you will need its actual computation graph. Run
+If you want to fine-tune SenseBERT, run
 ```python
-model, tokenizer = load_model('sensebert-base-uncased', session=self.session)  # or sensebert-large-uncased
+sensebert_model = SenseBert("sensebert-base-uncased", session=session)  # or sensebert-large-uncased
 ```
 
-Then ```model``` is an instance of ```SenseBertGraph```, and it holds both the required placeholders (```input_ids``` and ```input_mask```), as well as three output tensors: ```contextualized_embeddings```, ```mlm_logits``` and ```supersense_logits```. You can take any of these three tensors and build your graph on top of it. 
+```sensebert_model.model.input_ids``` and ```sensebert_model.model.input_mask``` are the model's tensorflow placeholders, and ```sensebert_model.model.contextualized_embeddings```, ```sensebert_model.model.mlm_logits``` and ```sensebert_model.model.supersense_logits``` are the output tensors. You can take any of these three tensors and build your graph on top of them. 
 
 
 ### Download SenseBERT to your local machine
@@ -62,17 +66,13 @@ Then ```model``` is an instance of ```SenseBertGraph```, and it holds both the r
 In order to avoid high latency, we recommend to download the model once to your local machine. Our code also supports initializations from local directories. 
 For that, you will need to install ```gsutil```. Once you have it, run one of the following
 ```shell script
-gsutil -m cp -r "gs://ai21-public-models/sensebert-base-uncased" PATH/TO/DIR
-gsutil -m cp -r "gs://ai21-public-models/sensebert-large-uncased" PATH/TO/DIR
+gsutil -m cp -r gs://ai21-public-models/sensebert-base-uncased PATH/TO/DIR
+gsutil -m cp -r gs://ai21-public-models/sensebert-large-uncased PATH/TO/DIR
 ```
 
 Then you can go ahead and use our code exactly as before, with
 ```python
 sensebert_model = SenseBERT("PATH/TO/DIR", session=session)
-```
-or
-```python
-model, tokenizer = load_model("PATH/TO/DIR", session=session)
 ```
 
 ## Citation 
--- sensebert.py
+++ sensebert.py
@@ -1,100 +1,94 @@
 import os
 from collections import namedtuple
-import numpy as np
 import tensorflow as tf
 
 from tokenization import FullTokenizer
 
-SenseBertGraph = namedtuple('SenseBertGraph',
-                            ('input_ids', 'input_mask', 'contextualized_embeddings', 'mlm_logits', 'supersense_losits'))
+_SenseBertGraph = namedtuple(
+    'SenseBertGraph',
+    ('input_ids', 'input_mask', 'contextualized_embeddings', 'mlm_logits', 'supersense_losits')
+)
 
-model_paths = {
+_MODEL_PATHS = {
     'sensebert-base-uncased': 'gs://ai21-public-models/sensebert-base-uncased',
     'sensebert-large-uncased': 'gs://ai21-public-models/sensebert-large-uncased'
 }
-contextualized_embeddings_tensor_name = "bert/encoder/Reshape_13:0"
+_CONTEXTUALIZED_EMBEDDINGS_TENSOR_NAME = "bert/encoder/Reshape_13:0"
 
 
-def load_model(name_or_path, session=None):
-    if name_or_path in model_paths:
+def _get_model_path(name_or_path):
+    if name_or_path in _MODEL_PATHS:
         print(f"Loading the known model '{name_or_path}'")
-        model_path = model_paths[name_or_path]
+        model_path = _MODEL_PATHS[name_or_path]
     else:
         print(f"This is not a known model. Assuming this is a path or a url...")
         model_path = name_or_path
+    return model_path
+
+
+def load_tokenizer(name_or_path):
+    model_path = _get_model_path(name_or_path)
+    vocab_file = os.path.join(model_path, "vocab.txt")
+    supersense_vocab_file = os.path.join(model_path, "supersense_vocab.txt")
+    return FullTokenizer(vocab_file=vocab_file, senses_file=supersense_vocab_file)
+
+
+def _load_model(name_or_path, session=None):
+    model_path = _get_model_path(name_or_path)
 
     if session is None:
         session = tf.get_default_session()
 
-    graph = session.graph
-    export_dir = os.path.dirname((tf.io.gfile.glob(os.path.join(model_path, 'variables')) +
-                                  tf.io.gfile.glob(os.path.join(model_path, '**', 'variables')))[0])
-    model = tf.saved_model.load(export_dir=export_dir, sess=session, tags=[tf.saved_model.SERVING])
+    model = tf.saved_model.load(export_dir=model_path, sess=session, tags=[tf.saved_model.SERVING])
     serve_def = model.signature_def[tf.saved_model.DEFAULT_SERVING_SIGNATURE_DEF_KEY]
 
-    inputs, outputs = ({key: graph.get_tensor_by_name(info.name) for key, info in puts.items()}
+    inputs, outputs = ({key: session.graph.get_tensor_by_name(info.name) for key, info in puts.items()}
                        for puts in (serve_def.inputs, serve_def.outputs))
-    outputs['contextualized_embeddings'] = session.graph.get_tensor_by_name(contextualized_embeddings_tensor_name)
 
-    vocab_file = os.path.join(model_path, "vocab.txt")
-    supersense_vocab_file = os.path.join(model_path, "supersense_vocab.txt")
-    tokenizer = FullTokenizer(vocab_file, senses_file=supersense_vocab_file)
-
-    input_ids, input_mask = inputs['input_ids'], inputs['input_mask']
-    embeddings, mlm, ss = outputs['contextualized_embeddings'], outputs['masked_lm'], outputs['ss']
-
-    return SenseBertGraph(input_ids=input_ids, input_mask=input_mask,
-                          contextualized_embeddings=embeddings, supersense_losits=ss, mlm_logits=mlm),  tokenizer
+    return _SenseBertGraph(
+        input_ids=inputs['input_ids'],
+        input_mask=inputs['input_mask'],
+        contextualized_embeddings=session.graph.get_tensor_by_name(_CONTEXTUALIZED_EMBEDDINGS_TENSOR_NAME),
+        supersense_losits=outputs['ss'],
+        mlm_logits=outputs['masked_lm']
+    )
 
 
 class SenseBert:
     def __init__(self, name_or_path, max_seq_length=512, session=None):
-        self.name_or_path = name_or_path
         self.max_seq_length = max_seq_length
         self.session = session if session else tf.get_default_session()
-
-        self.model, self.tokenizer = load_model(self.name_or_path, session=self.session)
-        self.model_output_tensors = [self.model.contextualized_embeddings,
-                                     self.model.mlm_logits, self.model.supersense_losits]
-
-        self.start_sym = self.tokenizer.start_sym
-        self.end_sym = self.tokenizer.end_sym
-        self.pad_sym_id = self.tokenizer.convert_tokens_to_ids([self.tokenizer.pad_sym])[0]
+        self.model = _load_model(name_or_path, session=self.session)
+        self.tokenizer = load_tokenizer(name_or_path)
 
     def tokenize(self, inputs):
         """
         Gets a string or a list of strings, and returns a tuple (input_ids, input_mask) to use as inputs for SenseBERT.
-        Both share the same shape: [batch_size, sequence_length]
-        :param inputs: A string or a list of string
+        Both share the same shape: [batch_size, sequence_length] where sequence_length is the maximal sequence length.
         """
         if isinstance(inputs, str):
             inputs = [inputs]
 
-        assert isinstance(inputs, list)
-        assert all([isinstance(s, str) for s in inputs])
-
-        # Tokenizing all inputs
+        # tokenizing all inputs
         all_token_ids = []
         for inp in inputs:
-            tokens = [self.start_sym] + self.tokenizer.tokenize(inp)[0] + [self.end_sym]
+            tokens = [self.tokenizer.start_sym] + self.tokenizer.tokenize(inp)[0] + [self.tokenizer.end_sym]
             assert len(tokens) <= self.max_seq_length
+            all_token_ids.append(self.tokenizer.convert_tokens_to_ids(tokens))
 
-            tokens = self.tokenizer.convert_tokens_to_ids(tokens)
-            all_token_ids.append(tokens)
-
-        # Deciding the maximum sequence length and padding accordingly
-        max_len = np.max([len(token_ids) for token_ids in all_token_ids])
+        # decide the maximum sequence length and pad accordingly
+        max_len = max([len(token_ids) for token_ids in all_token_ids])
         input_ids, input_mask = [], []
+        pad_sym_id = self.tokenizer.convert_tokens_to_ids([self.tokenizer.pad_sym])
         for token_ids in all_token_ids:
             to_pad = max_len - len(token_ids)
-            input_ids.append(token_ids + [self.pad_sym_id] * to_pad)
+            input_ids.append(token_ids + pad_sym_id * to_pad)
             input_mask.append([1] * len(token_ids) + [0] * to_pad)
 
         return input_ids, input_mask
 
     def run(self, input_ids, input_mask):
-        model_outputs = self.session.run(self.model_output_tensors,
-                                         feed_dict={self.model.input_ids: input_ids,
-                                                    self.model.input_mask: input_mask})
-
-        return model_outputs
+        return self.session.run(
+            [self.model.contextualized_embeddings, self.model.mlm_logits, self.model.supersense_losits],
+            feed_dict={self.model.input_ids: input_ids, self.model.input_mask: input_mask}
+        )
